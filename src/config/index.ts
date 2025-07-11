2efc50f88bf3e4c90a5c891a9988d129079ee454
gongsikai <gongsikai@189.cn>
2025-07-11 11:10:11 +0800

    add: add config

---
+++ src/config/index.ts
@@ -0,0 +1,6 @@
+export default {
+    webName: 'jiniubao-admin-react',
+    webVersion: '1.0.0.0.0',
+    apiBaseUrl: 'https://api.jiniubao.com/api',
+    apiBaseUrl2: 'https://api2.jiniubao.com/api2',
+}
